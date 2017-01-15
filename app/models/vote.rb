b9285d2f3f901f7ff60e1d04b54ea55a7cc97565
Chris Santos & Nick Devlin <pair+csan21.nickdevlin@devbootcamp.com>
2017-01-14 21:11:31 -0600

    Upvotes destroy old downvote and vice versa

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
   belongs_to :voter, foreign_key: :voter_id, class_name: "User"
 
-  validates_uniqueness_of :voter_id, :scope => [:votable_id, :votable_type]
+  validates_uniqueness_of :voter_id, :scope => [:votable_id, :votable_type, :upvote?]
 end
--- public/js/post.js
+++ public/js/post.js
@@ -4,16 +4,78 @@ $(document).ready(function() {
     event.preventDefault();
     var action = $(this).attr('action');
     var method = $(this).attr('method');
+    var form_data = $(this).serialize();
 
     $.ajax({
       method: method,
-      url: action
+      url: action,
+      data: form_data
     })
 
     .done(function(response) {
-      console.log($("#vote-post h3"));
+      $("h3").replaceWith(response);
 
     });
 
 	});
+
+  $('#downvote-post').on('submit', function(event) {
+    event.preventDefault();
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
+    var form_data = $(this).serialize();
+
+    $.ajax({
+      method: method,
+      url: action,
+      data: form_data
+    })
+
+    .done(function(response) {
+      $("h3").replaceWith(response);
+
+    });
+
+	});
+
+  $(document).on('submit', '#upvote-comment',function(event) {
+    event.preventDefault();
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
+    var form_data = $(this).serialize();
+
+    $.ajax({
+      method: method,
+      url: action,
+      data: form_data
+    })
+
+    .done(function(response) {
+      $("h4").replaceWith(response);
+
+    });
+
+	});
+
+  $(document).on('click', '#downvote-comment',function(event) {
+    event.preventDefault();
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
+    var form_data = $(this).serialize();
+
+    $.ajax({
+      method: method,
+      url: action,
+      data: form_data
+    })
+
+    .done(function(response) {
+      $("h4").replaceWith(response);
+
+    });
+
+	});
+
+
+
 });
